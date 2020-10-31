d0fd74f6a45e7dfd40b5b540d2e697dec757acd3
Emmanuel <etambele@gmail.com>
2020-10-31 12:11:23 -0400

    genesis

---
+++ src/main/java/com/example/Ttunes/TtunesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ttunes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtunesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtunesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Ttunes/TtunesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ttunes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtunesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
